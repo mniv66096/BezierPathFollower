51ead68581a5f88d432d4a2d13bb9e98020c6852
SiyuanQ <siyuan.qiu1@gmail.com>
2018-05-01 14:46:00 -0400

    First Commit

---
+++ PathFollower.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollower : MonoBehaviour {
+    public BezierCurve Path;
+
+    public void StartFollowingPath (float percentInSecond) {
+        StartCoroutine(FollowPath(percentInSecond));
+    }
+
+    private IEnumerator FollowPath (float percentInSecond) {
+        float percentage = 0.0f;
+        while (percentage < 1.0f) {
+            percentage += Time.deltaTime * percentInSecond;
+            percentage = Mathf.Clamp(percentage, 0.0f, 1.0f);
+            Vector3 point = Path.GetPointAt(percentage);
+            point.y = transform.position.y;
+            transform.forward = point - transform.position;
+            transform.position = point;
+            yield return null;  
+        }
+    }
+}
